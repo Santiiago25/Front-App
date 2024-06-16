a30be19ef087022f9378fc5de8dfb10aa821dfbc
alero911 <alero91@gmail.com>
2024-06-16 15:35:46 -0500

    front Login formulario inscripcion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule,FormControl } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { LoginServiceService } from './services/login-service.service';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { CreateUserComponent} from './create-user/create-user.component'
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators ,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
-  standalone: true,
-  imports: [],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
-export class CreateUserComponent {
 
+export class CreateUserComponent implements OnInit {
+  submitted = false;
+  inscripcionForm!: FormGroup;
+
+  usuarioAutenticado = 'Pepito'; // Simulación del nombre del usuario autenticado
+
+  tiposDocumento = ['DNI', 'Pasaporte', 'Cédula'];
+  sexos = ['Masculino', 'Femenino', 'Otro'];
+  estados = ['Activo', 'Inactivo'];
+  tiposUsuario = ['Administrador', 'Ejecutivo'];
+
+  get username() {
+    return this.formUser.get('username') as FormControl;
+  }
+  get password() {
+    return this.formUser.get('password') as FormControl;
+  }
+
+  formUser = new FormGroup({
+    'username': new FormControl('', Validators.required),
+    'password': new FormControl('', Validators.required)
+  });
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+  }  
+
+  onSubmit(): void {
+    
+  }
+  get f() {
+    return this.inscripcionForm.controls;
+  }
+
+  // Métodos simulados para las opciones del menú
+  crearUsuario() {
+    console.log('Crear usuario');
+  }
+
+  actualizarUsuario() {
+    console.log('Actualizar usuario');
+  }
 }
