61e5f87649fc1f64a6e58d10d4e379a6cbb0059c
alero911 <alero91@gmail.com>
2024-06-18 20:30:18 -0500

    front Login formulario inscripcion se implementa formulario reactivo

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -16,27 +16,63 @@ export class CreateUserComponent implements OnInit {
   tiposDocumento = ['DNI', 'Pasaporte', 'Cédula'];
   sexos = ['Masculino', 'Femenino', 'Otro'];
   estados = ['Activo', 'Inactivo'];
-  tiposUsuario = ['Administrador', 'Ejecutivo'];
+  tiposUsuarios = ['Administrador', 'Ejecutivo'];
 
-  get username() {
-    return this.formUser.get('username') as FormControl;
+  get nombres() {
+    return this.formUser.get('nombres') as FormControl;
   }
-  get password() {
-    return this.formUser.get('password') as FormControl;
+  get Apellidos() {
+    return this.formUser.get('Apellidos') as FormControl;
+  }
+  get tipoDocumento() {
+    return this.formUser.get('tipoDocumento') as FormControl;
+  }
+  get numeroDocumento() {
+    return this.formUser.get('numeroDocumento') as FormControl;
+  }
+  get sexo() {
+    return this.formUser.get('sexo') as FormControl;
+  }
+  get correoElectronico() {
+    return this.formUser.get('correoElectronico') as FormControl;
+  }
+  get estado() {
+    return this.formUser.get('estado') as FormControl;
+  }
+  get celular() {
+    return this.formUser.get('celular') as FormControl;
+  }
+  get tipoUsuario() {
+    return this.formUser.get('tipoUsuario') as FormControl;
   }
 
   formUser = new FormGroup({
-    'username': new FormControl('', Validators.required),
-    'password': new FormControl('', Validators.required)
+    'nombres': new FormControl('', Validators.required),
+    'Apellidos': new FormControl('', Validators.required),
+    'tipoDocumento': new FormControl('', Validators.required),
+    'numeroDocumento': new FormControl('', Validators.required),
+    'sexo': new FormControl('', Validators.required),
+    'correoElectronico': new FormControl('', [Validators.required,Validators.email]),
+    'estado': new FormControl('', Validators.required),
+    'celular': new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]),
+    'tipoUsuario': new FormControl('', Validators.required)
   });
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
   }  
 
   onSubmit(): void {
-    
+    this.submitted = true;
+
+    if (this.formUser.invalid) {
+      return;
+    }
+
+    console.log('Formulario enviado', this.formUser.value.nombres);
   }
+  
   get f() {
     return this.inscripcionForm.controls;
   }
@@ -48,5 +84,15 @@ export class CreateUserComponent implements OnInit {
 
   actualizarUsuario() {
     console.log('Actualizar usuario');
+  }
+
+  soloNumeros(event: any): void {
+    const pattern = /[0-9]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    
+    if (!pattern.test(inputChar)) {
+      // Detener la entrada de caracteres que no sean números
+      event.preventDefault();
+    }
   }
 }
